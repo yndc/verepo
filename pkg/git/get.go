7f79b680320e5aca46b655b7a33f090d02957190
yondercode <yondercode@gmail.com>
2022-03-16 12:55:36 +0700

    fix getting latest version

--- pkg/git/get.go
+++ pkg/git/get.go
@@ -13,14 +13,34 @@ func LatestVer(app string) semver.Parsed {
 		return semver.Invalid()
 	}
 
-	if len(o) == 0 {
-		return semver.Invalid()
+	if tag, ok := getLatestTag(o); ok {
+		return tag
 	}
+	return semver.Invalid()
+}
 
-	sp := strings.Split(string(o), "/")
-	if len(sp) != 2 {
-		return semver.Invalid()
+func getLatestTag(o []byte) (semver.Parsed, bool) {
+	sp := strings.Split(string(o), "\n")
+	found := false
+	latest := semver.Parsed{
+		Major:      0,
+		Minor:      0,
+		Patch:      0,
+		Prerelease: []string{"dev"},
 	}
-	v, _ := semver.Parse(strings.Trim(sp[1], "\n "))
-	return v
+	for _, s := range sp {
+		if len(s) == 0 {
+			continue
+		}
+		sp := strings.Split(s, "/")
+		p, err := semver.Parse(sp[1])
+		if err != nil {
+			continue
+		}
+		if semver.Compare(latest, p) > 0 {
+			latest = p
+			found = true
+		}
+	}
+	return latest, found
 }
